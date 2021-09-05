342717980caf04dabaf9d209f6025d422f40e8e7
Mark Sulimov <mark.sulimov@human-id.org>
2021-09-05 16:46:45 -0700

    Set night xp rate now constant across levels that are >= 8. Increased survivor XP requirement for level 12 to make getting grappling hook harder. Lowered legend level XP to reflect new night XP rate.
    Changed legend xp curve script to pause at end for user to enter something to allow copying of new levels
    Made k positive in logistic function

--- Data3/data/skills/legend_curve.py
+++ Data3/data/skills/legend_curve.py
@@ -1,19 +1,16 @@
 from math import e
 
 c = 0
-L = 1050000
+L = 510000
 x0 = 42.0
-k = 0.021
+k = -0.021
 
 def curve(i: int) -> int:
-    return int(c + L/(1 + pow(e, (-k*(i-x0)))))
+    return int(c + L/(1 + pow(e, (k*(i-x0)))))
 
 
 for i in range(1, 251):
     print(f"<level id=\"{i}\" type=\"Legend\">"
           f"<prop n=\"ExpToNextLevelLegend\" v=\"{curve(i)}\"/></level>")
 
-if __name__ == "main":
-    for i in range(1, 251):
-        print(f"<level id=\"{i}\" type=\"Legend\">"
-              f"<prop n=\"ExpToNextLevelLegend\" v=\"{curve(i)}\"/></level>")
+input() # let them copy level data
